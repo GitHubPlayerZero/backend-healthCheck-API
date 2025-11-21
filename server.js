5ab80e84e6769b02bc118d09faf239773450bd8d
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-11-21 13:10:36 +0800

    修改產品 ID

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const server = http.createServer((req, res) => {
       { id: 4, name: "耳機", price: 2990 },
       { id: 5, name: "螢幕", price: 6990 },
       { id: 6, name: "Dell大螢幕", price: 12990 },
-      { id: 6, name: "Zoo 量子筆電（帶你體驗量子世界）", price: 100000 },
+      { id: 7, name: "Zoo 量子筆電（帶你體驗量子世界）", price: 100000 },
     ];
 
     // 解析 URL 和 query 參數
